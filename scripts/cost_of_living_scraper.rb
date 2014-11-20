68a4f9138e46f83735e1266c6cca9bf919ea5a59
Junior Matelau <matelau@gmail.com>
2014-11-20 12:14:09 -0700

    added more areas and added error handlingw/log

--- scripts/cost_of_living_scraper.rb
+++ scripts/cost_of_living_scraper.rb
@@ -6,14 +6,23 @@ require 'pathname'
 city_to_values = Hash.new 
 state_to_values = Hash.new
 # TODO update to collect data on multiple cities in a less formal matter
-areas = ["phoenix-az", "tucson-az", "los+angeles-ca", "san+francisco-ca", "san+jose-ca", "sacramento-ca", "denver-co", "fort+lauderdale-fl", "new+york-ny", "austin-tx", "seattle-wa", "madison-wi", "salt+lake+city-ut"]
+areas = ["phoenix-az", "tucson-az", "mesa-az", "los+angeles-ca", "san+francisco-ca", "san+jose-ca", "san+diego-ca", "sacramento-ca", "denver-co", "colorado+springs-co", "aurora-co", "fort+lauderdale-fl", "jacksonville-fl", "miami-fl", "tampa-fl", "new+york-ny", "oyster+bay-ny", "buffalo-ny", "austin-tx", "houston-tx", "san+antonio-tx", "dallas-tx", "seattle-wa", "spokane-wa", "tacoma-wa", "vancouver-wa", "madison-wi", "milwaukee-wi", "green+bay-wi", "salt+lake+city-ut", "west+valley+city-ut", "provo-ut"]
 
 areas.each do |area|
 	# Test Script used to play with nokogiri - pulls cost of living data from areavibes.com
 	url = "http://www.areavibes.com//cost-of-living/"
 	url.insert(25,area)
 	puts "pulling data for: "+ url + "\n"
-	data = Nokogiri::HTML(open(url))
+	data = nil
+	begin
+		data = Nokogiri::HTML(open(url))
+	rescue OpenURI::HTTPError => e
+		log_file = Pathname.pwd.to_s + "/data/error_logs"
+		error_message = DateTime.now.to_s + " Cost_of_living_scraper.rb area: "+ area + " error: "+ e.message.to_s
+		File.write(log_file, error_message)
+		# continue
+		next
+	end
 
 	table =  data.css('table.std_facts.w')
 	#headers =  index, city, state, national
@@ -48,16 +57,17 @@ areas.each do |area|
 	city_to_values[city] = columns_to_values 
 
 
-	if state_to_values.has_key?(state)
+	if state_to_values.key?(state)
 		# do nothing
 	else
+		# map state vals to columns	
 		columns_to_values = Hash.new
 		columns.each_with_index do |col, x|
 			columns_to_values[col] = state_values[x]
 		end
 		state_to_values[state] = columns_to_values
 	end
-	# map state vals to columns	
+
 end
 col_file = Pathname.pwd.to_s + "/data/col.json"
 col_state_file = Pathname.pwd.to_s + "/data/col_state.json"
