097eca94d02af82c49e8ff7a36223a5b959ef9a3
Junior Matelau <matelau@gmail.com>
2014-11-21 19:19:40 -0700

    stubbed out dbvals in dbscript and added Gemfile

--- scripts/db_script.rb
+++ scripts/db_script.rb
@@ -1,10 +1,24 @@
-require 'mysql2'
+# require 'mysql2'
+require 'yaml'
+require 'pathname'
+
+path = Pathname.pwd.to_s
+path.sub! 'scripts', 'config/database.yml'
+puts path
+db_vals = YAML.load_file(path)
+values = db_vals["development"]
+@host_ip = values["host"]
+@port = values["port"]
+@username = values["username"]
+@password = values["password"]
+@database_name = values["database"]
 
 db_client = Mysql2::Client.new(
-  :host => "#{host_ip}", 
-  :username => "#{username}",
-  :password => "#{password}",
-  :database => "#{database_name}")
+  :host => "#{@host_ip}", 
+  :port => "#{@port}",
+  :username => "#{@username}",
+  :password => "#{@password}",
+  :database => "#{@database_name}")
 
-query = "SELECT col_name FROM table"
-results = db_client.query(query)
+# query = "SELECT col_name FROM table"
+# results = db_client.query(query)
