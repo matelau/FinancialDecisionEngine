cb2434fb876127a4300c0cc36e8bb43048279a9e
Junior Matelau <matelau@gmail.com>
2014-11-23 13:06:48 -0700

    added switch to write json or push to db within script

--- scripts/cost_of_living_scraper.rb
+++ scripts/cost_of_living_scraper.rb
@@ -7,10 +7,19 @@ require 'yaml'
 
 # init maps and necessary array
 loc_to_values = Hash.new 
-state_to_values = Hash.new
+processed_states = Array.new
 weather_area = Hash.new
 columns = Array.new
 cities = Array.new
+
+####### !!!!!!!!!!!!!!
+####### !!!!!!!!!!!!!!
+#------------------- Make Sure You Set write_json val to false to push to db ---------------------------
+# switch to write json else pushes values to db
+write_json = true
+####### !!!!!!!!!!!!!!
+####### !!!!!!!!!!!!!!
+####### !!!!!!!!!!!!!!
 
 # TODO update to collect data on multiple cities in a less formal matter
 areas = ["phoenix-az" , "tucson-az" , "mesa-az", "los+angeles-ca", "san+francisco-ca", "san+jose-ca", "san+diego-ca", "sacramento-ca", "denver-co", "colorado+springs-co", "aurora-co","washington-dc", "fort+lauderdale-fl", "jacksonville-fl", "miami-fl", "tampa-fl", "chicago-il", "aurora-il", "indianapolis-in", "boston-ma", "detroit-mi", "columbus-oh", "charlotte-nc", "new+york-ny", "oyster+bay-ny", "buffalo-ny", "philadelphia-pa", "memphis-tn", "nashville-tn", "austin-tx","el+paso-tx", "fort+worth-tx", "houston-tx", "san+antonio-tx", "dallas-tx", "seattle-wa", "spokane-wa", "tacoma-wa", "vancouver-wa", "madison-wi", "milwaukee-wi", "green+bay-wi", "salt+lake+city-ut", "west+valley+city-ut", "provo-ut"]
@@ -62,17 +71,26 @@ areas.each do |area|
 	end
 	# include general state col in data
 	# columns_to_values[state] = state_values[0]
-	loc_to_values[city] = columns_to_values 
+	str = city
+	if write_json
+		str = "location:#{city}"
+	end
+	loc_to_values[str] = columns_to_values 
 
-	if state_to_values.key?(state)
+	if processed_states.include?(state)
 		# do nothing
 	else
 		# map state vals to columns	
+		processed_states.push(state)
 		columns_to_values = Hash.new
 		columns.each_with_index do |col, x|
 			columns_to_values[col] = state_values[x]
 		end
-		loc_to_values[state] = columns_to_values
+		str = state
+		if write_json
+			str = "location':'#{state}"
+		end
+		loc_to_values[str] = columns_to_values
 	end
 
 	#-------------------Weather Data ----------------------------------
@@ -131,93 +149,90 @@ areas.each do |area|
 	weather_area[city] = month_data 
 
 end
-# ------------------ Database init ----------------------------------
-# get relative path
-path = Pathname.pwd.to_s
-path.sub! 'scripts', 'config/database.yml'
-db_vals = YAML.load_file(path)
-values = db_vals["development"]
-@host_ip = values["host"]
-@port = values["port"]
-@username = values["username"]
-@password = values["password"]
-@database_name = values["database"]
 
-db_client = Mysql2::Client.new(
-  :host => "#{@host_ip}", 
-  :port => "#{@port}",
-  :username => "#{@username}",
-  :password => "#{@password}",
-  :database => "#{@database_name}",
-  :secure_auth => false)
+if write_json
+	# -------------------- Write Json ---------------------------
+	# store_vals
+	col_file = Pathname.pwd.to_s + "/data/col.json"
+	# col_state_file = Pathname.pwd.to_s + "/data/col_state.json"
+	js = loc_to_values.to_json
+	File.write(col_file, js)
 
-# ------------------ Database Code COL -------------------------------
+else
+	# ------------------ Database init ----------------------------------
+	get relative path
+	path = Pathname.pwd.to_s
+	path.sub! 'scripts', 'config/database.yml'
+	db_vals = YAML.load_file(path)
+	values = db_vals["development"]
+	@host_ip = values["host"]
+	@port = values["port"]
+	@username = values["username"]
+	@password = values["password"]
+	@database_name = values["database"]
 
-store_vals = Hash.new
-table_name = "colis"
-store_vals[table_name] = loc_to_values
-curr_loc = " "
-col = " "
-goods = " "
-groc = " "
-hc = " "
-housing = " "
-trans = " "
-util = " "
+	db_client = Mysql2::Client.new(
+	  :host => "#{@host_ip}", 
+	  :port => "#{@port}",
+	  :username => "#{@username}",
+	  :password => "#{@password}",
+	  :database => "#{@database_name}",
+	  :secure_auth => false)
 
+	# ------------------ Database Code COL -------------------------------
 
-store_vals[table_name].each do |loc|
-	curr_loc =  loc[0].to_s
-	loc_data =loc_to_values[curr_loc]
-	col = loc_data[columns[0]]
-	goods = loc_data[columns[1]]
-	groc = loc_data[columns[2]]
-	hc = loc_data[columns[3]]
-	housing = loc_data[columns[4]]
-	trans = loc_data[columns[5]]
-	util = loc_data[columns[6]]
-	query = "REPLACE INTO cents_dev.colis (cost_of_living, transportation, groceries, goods, health_care, utilities, location, housing) VALUES (#{col},#{trans},#{groc},#{goods},#{hc},#{util}, '#{curr_loc}', #{housing})"
-	results =  db_client.query(query)
-end
+	store_vals = Hash.new
+	table_name = "colis"
+	store_vals[table_name] = loc_to_values
+	curr_loc = " "
+	col = " "
+	goods = " "
+	groc = " "
+	hc = " "
+	housing = " "
+	trans = " "
+	util = " "
 
 
-# --------------- Database Code weather_reports ------------------------- 
-
-cities.each do |city|
-	# get id
-	query = "SELECT id FROM colis WHERE location='#{city}'"
-	results = db_client.query(query)
-	id = nil
-	results.each do |row|
-		id = row["id"]
+	store_vals[table_name].each do |loc|
+		curr_loc =  loc[0].to_s
+		loc_data =loc_to_values[curr_loc]
+		col = loc_data[columns[0]]
+		goods = loc_data[columns[1]]
+		groc = loc_data[columns[2]]
+		hc = loc_data[columns[3]]
+		housing = loc_data[columns[4]]
+		trans = loc_data[columns[5]]
+		util = loc_data[columns[6]]
+		query = "REPLACE INTO cents_dev.colis (cost_of_living, transportation, groceries, goods, health_care, utilities, location, housing) VALUES (#{col},#{trans},#{groc},#{goods},#{hc},#{util}, '#{curr_loc}', #{housing})"
+		results =  db_client.query(query)
 	end
 
-	if id != nil
-		# iterate through months and push data to db
-		weather_area[city].each do |months|
-			# get month
-			curr_month = months[0]
-			# get values
-			arr_vals = months[1]
-			min = arr_vals[0]
-			max = arr_vals[1]
-			avg = arr_vals[2]
-			query = "REPLACE INTO cents_dev.weather_records (high, low, average, coli_id, month) VALUES (#{max}, #{min}, #{avg}, #{id}, '#{curr_month}')"
-			results = db_client.query(query)
+
+	# --------------- Database Code weather_reports ------------------------- 
+
+	cities.each do |city|
+		# get id
+		query = "SELECT id FROM colis WHERE location='#{city}'"
+		results = db_client.query(query)
+		id = nil
+		results.each do |row|
+			id = row["id"]
+		end
+
+		if id != nil
+			# iterate through months and push data to db
+			weather_area[city].each do |months|
+				# get month
+				curr_month = months[0]
+				# get values
+				arr_vals = months[1]
+				min = arr_vals[0]
+				max = arr_vals[1]
+				avg = arr_vals[2]
+				query = "REPLACE INTO cents_dev.weather_records (high, low, average, coli_id, month) VALUES (#{max}, #{min}, #{avg}, #{id}, '#{curr_month}')"
+				results = db_client.query(query)
+			end
 		end
 	end
 end
-
-
-	
-# -------------------- Write Json ---------------------------
-# store_vals
-# col_file = Pathname.pwd.to_s + "/data/col.json"
-# # col_state_file = Pathname.pwd.to_s + "/data/col_state.json"
-# js = store_vals.to_json
-# File.write(col_file, js)
-# js = state_to_values.to_json
-# File.write(col_state_file, js)
-
-
-# TODO implement logic to store values to db
